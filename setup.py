3dcde555d9dc0a5eeafe01b355bef43f615e0c07
Evan <105789570+xufuyu@users.noreply.github.com>
2024-07-02 21:58:45 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='PowerBlur',
     packages=find_packages(),
-    version='0.0.1'
+    version='0.0.1',
     install_requires=['numpy','pillow']
 )
